e9db624cfc68e1eb8cc1b06156b11815eec801f6
amilisavljevic <amilisavljevic@fer.hr>
2019-06-05 17:42:40 +0200

    results for 10 randomly selected datasets for each number of nodes (5 to 10)

--- src/Parsimony.java
+++ src/Parsimony.java
@@ -13,82 +13,98 @@ public class Parsimony {
 
     private static Path topologies = Paths.get("resources/topologies.txt");
 
+    private static String random = "resources/random_data/prolazak%d/random%d.fasta";
+    private static String first = "resources/first%d.fasta";
+
+    private static String resultTreeFile = "results/prolazak%d/tree%d.txt";
+
     public static void main(String[] args) {
-        for (int h = 5; h < 11; h++) {
-            Path infile = Paths.get(String.format("resources/first%d.fasta", h));
+        for (int g = 1; g < 11; g++){
 
-            List<List<Character>> matrix = new ArrayList<>();
-            Map<Integer, String> names = new HashMap<>();
+            for (int h = 5; h < 11; h++) {
+//                Path infile = Paths.get(String.format("resources/first%d.fasta", h));
+                Path infile = Paths.get(String.format(random, g, h));
 
-            System.out.println("Reading file: " + infile);
+                List<List<Character>> matrix = new ArrayList<>();
+                Map<Integer, String> names = new HashMap<>();
 
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader(infile.toString()));
-                String line = reader.readLine();
+                System.out.println("Reading file: " + infile);
 
-                int i = 0;
-                while (line != null) {
-                    names.put(i++, line.substring(1));
-                    line = reader.readLine();
-                    matrix.add(line.chars().mapToObj(c -> (char) c).collect(Collectors.toList()));
-                    line = reader.readLine();
-                }
+                try {
+                    BufferedReader reader = new BufferedReader(new FileReader(infile.toString()));
+                    String line = reader.readLine();
 
-                reader.close();
-            } catch (Exception ex) {
-                System.out.println("Cannot read file");
-                System.exit(1);
-            }
-
-            long startTime = System.currentTimeMillis();
-
-            filterColumns(matrix);
-
-            int informativeSites = matrix.get(0).size();
-            int treesSize = generateTrees(matrix.size());
-
-            System.gc();
-            List<Integer> results = new LinkedList<>();
-
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader(topologies.toString()));
-
-                for (int i = 0; i < treesSize; i++) {
-                    String treeString = reader.readLine();
-                    int sum = 0;
-                    for (int j = 0; j < informativeSites; j++) {
-                        for (int k = 0; k < matrix.size(); k++) {
-                            treeString = treeString.replace(String.format(" %d ", k), String.format(" %c ", matrix.get(k).get(j)));
-                        }
-                        Tree tree = Tree.parse(treeString);
-                        sum += tree.getChanges();
+                    int i = 0;
+                    while (line != null) {
+                        names.put(i++, line.substring(1));
+                        line = reader.readLine();
+                        matrix.add(line.chars().mapToObj(c -> (char) c).collect(Collectors.toList()));
+                        line = reader.readLine();
                     }
-                    results.add(sum);
+
+                    reader.close();
+                } catch (Exception ex) {
+                    System.out.println("Cannot read file");
+                    System.exit(1);
                 }
 
-            } catch (IOException ex) {
-                System.out.println("Cannot find file with topologies!");
-                System.exit(1);
-            }
+                long startTime = System.currentTimeMillis();
 
-            long endTime = System.currentTimeMillis();
-            int index = results.indexOf(results.stream().min(Comparator.comparingInt(Integer::intValue)).get());
-            System.out.println(String.format("Number of changes: %d", results.get(index)));
-            System.out.println(String.format("The best tree is tree %d ", index));
+                filterColumns(matrix);
 
+                int informativeSites = matrix.get(0).size();
+                int treesSize = generateTrees(matrix.size());
 
-            String tree;
-            try (Stream<String> lines = Files.lines(topologies)) {
-                tree = lines.skip(index).findFirst().get();
-                for (int k = 0; k < matrix.size(); k++) {
-                    tree = tree.replace(String.format(" %d ", k), String.format(" %s ", names.get(k)));
+                System.gc();
+                List<Integer> results = new LinkedList<>();
+
+                try {
+                    BufferedReader reader = new BufferedReader(new FileReader(topologies.toString()));
+
+                    for (int i = 0; i < treesSize; i++) {
+                        String treeString = reader.readLine();
+                        int sum = 0;
+                        for (int j = 0; j < informativeSites; j++) {
+                            for (int k = 0; k < matrix.size(); k++) {
+                                treeString = treeString.replace(String.format(" %d ", k), String.format(" %c ", matrix.get(k).get(j)));
+                            }
+                            Tree tree = Tree.parse(treeString);
+                            sum += tree.getChanges();
+                        }
+                        results.add(sum);
+                    }
+
+                } catch (IOException ex) {
+                    System.out.println("Cannot find file with topologies!");
+                    System.exit(1);
                 }
-                System.out.println(tree);
-            } catch (IOException ex) {
-                System.exit(1);
+
+                long endTime = System.currentTimeMillis();
+                int index = results.indexOf(results.stream().min(Comparator.comparingInt(Integer::intValue)).get());
+                System.out.println(String.format("Number of changes: %d", results.get(index)));
+                System.out.println(String.format("The best tree is tree %d ", index));
+
+
+                String tree;
+                try (Stream<String> lines = Files.lines(topologies)) {
+                    tree = lines.skip(index).findFirst().get();
+                    for (int k = 0; k < matrix.size(); k++) {
+                        tree = tree.replace(String.format(" %d ", k), String.format(" %s ", names.get(k)));
+                    }
+                    System.out.println(tree);
+
+                    OutputStream out = new BufferedOutputStream(Files.newOutputStream(
+                            Paths.get(String.format(resultTreeFile, g, h)), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING));
+                    out.write(tree.getBytes());
+                    out.flush();
+                    out.close();
+                } catch (IOException ex) {
+                    System.exit(1);
+                }
+
+                System.out.println(String.format("Time in millis: %d \n\n\n", endTime - startTime));
             }
 
-            System.out.println(String.format("Time in millis: %d \n\n\n", endTime - startTime));
         }
     }
 
