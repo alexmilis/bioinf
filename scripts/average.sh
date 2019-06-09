afd6c8a9b7f0ddb24f0ee0d6fdc4d89c792b3c3b
amilisavljevic <amilisavljevic@fer.hr>
2019-06-09 23:40:15 +0200

    script for sum of differences for n from 5 to 10

--- scripts/average.sh
+++ scripts/average.sh
@@ -10,14 +10,14 @@ for i in $(seq 1 10)
 do	
 	for j in $(seq 5 10)
 	do	
-		echo tail -n 1 "../results/prolazak$i/diff$j" | sed 's/[^0-9]*//g'
-		$number=$(tail -n 1 "../results/prolazak$i/diff$j" | sed 's/[^0-9]*//g')
+		number=$(tail -n 1 "../results/prolazak$i/diff$j" | grep -Eo '[0-9]+$')
 		a=${array[$j]}
-		$array[$j]=$(($a+$number))
+		array[$j]=$(($(($a))+$(($number))))
 	done
 done
 
 for i in $(seq 5 10)
 do
-	echo ${array[$i]}
+	echo $((${array[$i]}))
 done
+
